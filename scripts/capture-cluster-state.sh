b035a53250c15f3ae5d5563f54ac1d650ee04b45
Larry Steck <lsteck@us.ibm.com>
2020-07-28 15:56:38 -0500

    change chart deploy to use kubeclt

--- scripts/capture-cluster-state.sh
+++ scripts/capture-cluster-state.sh
@@ -7,10 +7,14 @@ EXCLUSIONS="$4"
 
 OUTFILE_DIR=$(dirname "${OUTFILE}")
 
+clusterType="${PLATFORM}"
+
 mkdir -p "${OUTFILE_DIR}"
 
 resources="deployment,statefulset,service,ingress,configmap,secret,serviceaccount"
 if [[ "$PLATFORM" == "openshift" ]] || [[ "$PLATFORM" == "ocp3" ]] || [[ "$PLATFORM" == "ocp4" ]]; then
+  clusterType="openshift"
+
   resources="${resources},route"
 
   if [[ "$PLATFORM" == "ocp4" ]]; then
@@ -21,6 +25,12 @@ fi
 echo "Checking on namespace - ${NAMESPACE}"
 
 if kubectl get namespace "${NAMESPACE}" 1> /dev/null 2> /dev/null; then
+
+  echo "deploy tool-test chart to namespace ${NAMESPACE}"
+  helm template mytest tool-test \
+    --repo "https://ibm-garage-cloud.github.io/toolkit-charts/" \
+    --version 0.1.0  \
+    --set global.clusterType=${clusterType} |  kubectl apply -n ${NAMESPACE} -f -
 
   echo "Listing resources in namespace - ${resources}"
   RESOURCES=$(kubectl get -n "${NAMESPACE}" "${resources}" -o jsonpath='{range .items[*]}{.metadata.namespace}{"/"}{.kind}{"/"}{.metadata.name}{"\n"}{end}' | tr '[:upper:]' '[:lower:]')
