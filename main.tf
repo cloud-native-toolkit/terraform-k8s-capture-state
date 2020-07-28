799b5b2497540c32c8b8365ed28d9facd0e28ea2
Larry Steck <lsteck@us.ibm.com>
2020-07-28 15:56:38 -0500

    change chart deploy to use kubeclt
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- main.tf
+++ main.tf
@@ -3,8 +3,12 @@ locals {
   outfile = "${var.output_path}/${var.namespace}.out"
 }
 
-resource null_resource write_kubeconfig {
+resource "null_resource" "capture_state" {
   provisioner "local-exec" {
-    command = "echo -n '${var.cluster_config_file_path}' > ${path.cwd}/.kubeconfig"
+    command = "${path.module}/scripts/capture-cluster-state.sh ${var.cluster_type} ${var.namespace} ${local.outfile} ${join(",", var.exclude_resources)}"
+
+    environment = {
+      KUBECONFIG = var.cluster_config_file_path
+    }
   }
 }
