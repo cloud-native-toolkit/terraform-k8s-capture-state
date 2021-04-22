e1bff13fd8b2c0871699e4d67d538f5bf06b6fdc
Sean Sundberg <seansund@us.ibm.com>
2021-04-22 09:11:31 -0500

    Remove current state output

--- main.tf
+++ main.tf
@@ -8,13 +8,3 @@ resource null_resource write_kubeconfig {
     command = "echo -n '${var.cluster_config_file_path}' > ${path.cwd}/.kubeconfig"
   }
 }
-
-resource "null_resource" "capture_state" {
-  provisioner "local-exec" {
-    command = "${path.module}/scripts/capture-cluster-state.sh ${var.cluster_type} ${var.namespace} ${local.outfile} ${join(",", var.exclude_resources)}"
-
-    environment = {
-      KUBECONFIG = var.cluster_config_file_path
-    }
-  }
-}
