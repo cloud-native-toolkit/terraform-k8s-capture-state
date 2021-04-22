8db791bd07c84e51358678da31dae58d104f9c8e
Sean Sundberg <seansund@us.ibm.com>
2021-04-21 19:12:17 -0500

    Writes kube config path out to .kubeconfig

--- main.tf
+++ main.tf
@@ -3,6 +3,12 @@ locals {
   outfile = "${var.output_path}/${var.namespace}.out"
 }
 
+resource null_resource write_kubeconfig {
+  provisioner "local-exec" {
+    command = "echo -n '${var.cluster_config_file_path}' > ${path.cwd}/.kubeconfig"
+  }
+}
+
 resource "null_resource" "capture_state" {
   provisioner "local-exec" {
     command = "${path.module}/scripts/capture-cluster-state.sh ${var.cluster_type} ${var.namespace} ${local.outfile} ${join(",", var.exclude_resources)}"
