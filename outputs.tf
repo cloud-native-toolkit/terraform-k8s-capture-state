6deb9d4151fa73a49fdc7226984027c2d2cfb6d4
Sean Sundberg <seansund@us.ibm.com>
2021-04-22 09:27:41 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "namespace" {
   value = var.namespace
-  depends_on = [null_resource.capture_state]
+  depends_on = [null_resource.write_kubeconfig]
 }
 
 output "outfile" {
   value = local.outfile
-  depends_on = [null_resource.capture_state]
+  depends_on = [null_resource.write_kubeconfig]
 }
 
 output "cluster_type" {
   value = var.cluster_type
-  depends_on = [null_resource.capture_state]
+  depends_on = [null_resource.write_kubeconfig]
 }
